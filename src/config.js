80e3b25e63638b4e97a5c573f30249624d63ff4d
Sakshi <scjuly69@gmail.com>
2020-07-28 18:49:53 +0530

    config change

--- src/config.js
+++ src/config.js
@@ -1,11 +1,11 @@
-// import Firebase from 'firebase';
-// let config = {
-//   apiKey: "AIzaSyBtQKKU23TX-z0bfth4joI4VGRVQDgivk8",
-//   authDomain: "demofirebase-e0dcf.firebaseapp.com",
-//   databaseURL: "https://demofirebase-e0dcf.firebaseio.com",
-//   projectId: "demofirebase-e0dcf",
-//   storageBucket: "demofirebase-e0dcf.appspot.com",
-//   messagingSenderId: "856400541201"
-// };
-// let app = Firebase.initializeApp(config);
-// export const db = app.database();
+import Firebase from 'firebase';
+let config = {
+  apiKey: "AIzaSyBtQKKU23TX-z0bfth4joI4VGRVQDgivk8",
+  authDomain: "demofirebase-e0dcf.firebaseapp.com",
+  databaseURL: "https://demofirebase-e0dcf.firebaseio.com",
+  projectId: "demofirebase-e0dcf",
+  storageBucket: "demofirebase-e0dcf.appspot.com",
+  messagingSenderId: "856400541201"
+};
+let app = Firebase.initializeApp(config);
+export const db = app.database();
